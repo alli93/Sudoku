52c7893e158a43e4fb4093223f904627bbdf5233
Ingimar Örn Oddsson <ingimar9@gmail.com>
2016-03-31 20:44:40 +0000

    Added toString() for State

--- src/State.java
+++ src/State.java
@@ -1,36 +1,84 @@
 import java.util.ArrayList;
 
-public class State {
+public class State
+{
 	public Grid board;
 	public int numOfNumbers;
-	
-	public class Grid {
+
+	public class Grid
+	{
 		ArrayList<ArrayList<Variable>> grid = new ArrayList<ArrayList<Variable>>();
-		
-		public class Variable {
+
+		public class Variable
+		{
 			public int domainSize;
 			public int assignment;
 			ArrayList<Integer> validAssignments = new ArrayList<Integer>();
-			
-			public Variable() {
+
+			public Variable()
+			{
 				domainSize = numOfNumbers;
 				assignment = 0;
-				for(int i = 1; i <= domainSize; i++) {
+				for (int i = 1; i <= domainSize; i++)
+				{
 					validAssignments.add(i);
 				}
 			}
+
 		}
-		public Grid(int numOfNumbers){
-			for(int i = 0; i < numOfNumbers; i++) {
+
+		public Grid(int numOfNumbers)
+		{
+			for (int i = 0; i < numOfNumbers; i++)
+			{
 				grid.add(new ArrayList<Variable>());
-				for(int j = 0; j < numOfNumbers; j++) {
+				for (int j = 0; j < numOfNumbers; j++)
+				{
 					grid.get(i).add(new Variable());
 				}
 			}
 		}
 	}
-	public State(int numOfNumbers) {
+
+	public State(int numOfNumbers)
+	{
 		this.numOfNumbers = numOfNumbers;
 		this.board = new Grid(numOfNumbers);
+	}
+
+	@Override
+	public String toString()
+	{
+		StringBuffer board = new StringBuffer();
+
+		for (int j = 0; j < numOfNumbers; ++j)
+		{
+			board.append("+---");
+		}
+		board.append("+\n");
+		for (int i = numOfNumbers - 1; i >= 0; --i)
+		{
+			for (int j = 0; j < numOfNumbers; ++j)
+			{
+
+				board.append("| " + this.board.grid.get(i).get(j).assignment + " ");
+
+			}
+			board.append("| \n");
+			if (i != 0)
+			{
+				for (int j = 0; j < numOfNumbers; ++j)
+				{
+					board.append("+---");
+				}
+				board.append("+\n");
+			}
+		}
+		for (int j = 0; j < numOfNumbers; ++j)
+		{
+			board.append("+---");
+		}
+		board.append("+\n");
+		return (board.toString());
 	}
 }
