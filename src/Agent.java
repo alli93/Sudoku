3257e3ff50a566d7714002a3047c007251ed74a2
Ingimar Örn Oddsson <ingimar9@gmail.com>
2016-03-31 20:25:34 +0000

    Started to implement init()

--- src/Agent.java
+++ src/Agent.java
@@ -1,5 +1,11 @@
-
 public class Agent
 {
-
+	private State startState;
+	// Fixed size while developing
+	private int numOfNumbers = 9;
+	
+	public void init()
+	{
+		startState = new State(numOfNumbers);
+	}
 }
