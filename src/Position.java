d4e828bd68807a9e0a5330dea897aac93ec53631
Ingimar Örn Oddsson <ingimar9@gmail.com>
2016-04-03 14:18:15 +0000

    equals() for Position

--- src/Position.java
+++ src/Position.java
@@ -1,15 +1,43 @@
+import java.util.Arrays;
 
-public class Position {
+public class Position
+{
 	public int row, column;
-	
-	public Position(int row, int column) {
+
+	public Position(int row, int column)
+	{
 		this.row = row;
 		this.column = column;
 	}
-	public int getRow() {
+
+	public int getRow()
+	{
 		return row;
 	}
-	public int getColumn() {
+
+	public int getColumn()
+	{
 		return column;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Position other = (Position) obj;
+		if (this.column != other.column)
+		{
+			return false;
+		}
+		if (this.row != other.row)
+		{
+			return false;
+		}
+		return true;
 	}
 }
