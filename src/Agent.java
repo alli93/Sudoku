8ba6041f5df5b4082f81dfe1280ed09345c7d2da
Ingimar Örn Oddsson <ingimar9@gmail.com>
2016-04-03 16:56:31 +0000

    Implemented csp_backtracking()

--- src/Agent.java
+++ src/Agent.java
@@ -1,12 +1,18 @@
+import java.util.ArrayList;
+
 public class Agent
 {
 	private State startState;
+	private State startState2;
 	// Fixed size while developing
 	private int numOfNumbers = 9;
+	
+	private int numOfNumbers2 = 4;
 
 	public void init()
 	{
 		startState = new State(numOfNumbers);
+		startState2 = new State(numOfNumbers2);
 
 		// Initialize a sudoku puzzle, easy difficulty
 		// Hardcoded until generator is implemented
@@ -45,13 +51,29 @@ public class Agent
 		startState.assignVariable(new Position(8, 3), 5);
 		startState.assignVariable(new Position(8, 4), 7);
 		startState.assignVariable(new Position(8, 7), 4);
+		startState.cellsChangedInForwardChecking.clear();		
 
-		System.out.println(startState);
+		/*System.out.println(startState.board.grid.get(0).get(0).validAssignments);
+		System.out.println(startState.numOfValuesRemovedByAssignment(new Position(0, 0), 5));
+		System.out.println(startState.numOfValuesRemovedByAssignment(new Position(0, 0), 7));
+		System.out.println(startState.numOfValuesRemovedByAssignment(new Position(0, 0), 8));
+		System.out.println(startState.leastConstrainingValues(new Position(0, 0)));
+		System.out.println(startState.leastConstrainingValues(new Position(2, 5)));*/
+		
+		startState2.assignVariable(new Position(0, 0), 3);
+		startState2.assignVariable(new Position(1, 1), 4);
+		startState2.assignVariable(new Position(2, 2), 2);
+		startState2.assignVariable(new Position(3, 1), 3);
+		startState2.assignVariable(new Position(3, 3), 1);
+		startState2.cellsChangedInForwardChecking.clear();
 
 	}
 
 	public State solvePuzzle()
 	{
+		System.out.println("Puzzle:");
+		System.out.println(startState);
+		System.out.println();
 		State solution = csp_backtracking(startState);
 		return solution;
 	}
@@ -63,12 +85,35 @@ public class Agent
 		{
 			return currentState;
 		}
-		
+
 		// Get the position of the most constrained variable
 		Position posOfMostConstrainedVariable = currentState.positionOfMostConstrainedVariable();
+
+		// Order the valid assigments according to the least-constraining-value
+		// heuristic
+		ArrayList<Integer> values = currentState.leastConstrainingValues(posOfMostConstrainedVariable);
 		
+
+		for (int i = 0; i < values.size(); i++)
+		{
+			State result = State.newInstance(currentState);
+			result.cellsChangedInForwardChecking.clear();
+			result.assignVariable(posOfMostConstrainedVariable, values.get(i));
+			
+			// Add value to variable and do forward checking
+			currentState.assignVariable(posOfMostConstrainedVariable, values.get(i));
 		
-		
+			if (!currentState.unassignedVariableHasAnEmptyDomain())
+			{
+				result = csp_backtracking(result);
+				if (result.isGoalState())
+				{
+					return result;
+				}
+			}
+			currentState.unAssignVariable(posOfMostConstrainedVariable, values.get(i));
+		}
+
 		return currentState;
 	}
 
